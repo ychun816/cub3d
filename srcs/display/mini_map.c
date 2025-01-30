8d47f81ad3e92141d48b161070bb3dcce34d39b6
Yi Chun Lin <yilin@paul-f3Br6s3.clusters.42paris.fr>
2025-01-30 21:07:26 +0100

    accept meeee why not show on github try again

--- srcs/display/mini_map.c
+++ srcs/display/mini_map.c
@@ -6,7 +6,7 @@
 /*   By: yilin <yilin@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 15:59:37 by ahadj-ar          #+#    #+#             */
-/*   Updated: 2025/01/30 21:00:21 by yilin            ###   ########.fr       */
+/*   Updated: 2025/01/30 21:06:40 by yilin            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,5 @@
 testest~yoyo
 ahhh im hungryyyy wanna have pizza
 maybe kebab or durum is good too!
+
+kong xi fa cai hotpot is good too!
