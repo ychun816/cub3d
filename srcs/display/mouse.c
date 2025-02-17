d9744e7d66e565754d9973f5d39b8e9872e8f144
Yi-Chun LIN <105208178+ychun816@users.noreply.github.com>
2025-02-17 17:16:45 +0000

    added mouse move BUT accidentally close the win omggg naurr

---
+++ srcs/display/mouse.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minimap.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yilin <yilin@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 15:59:37 by ahadj-ar          #+#    #+#             */
+/*   Updated: 2025/02/15 20:56:31 by yilin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/cub3d.h"
+
+/** mose_move 
+- no move if at center
+- y tronpi!! only check x -> rotate left/right
+- @note 
+- -> need to reset to center -> standard game move control & prevent mouse moving to edge!!!
+*/
+int mouse_move(int x, int y, t_cube *cube)
+{
+    if (x == W_WIDTH / 2 && y == W_HEIGHT / 2)
+        return (0);
+    //left
+    if (x < W_WIDTH / 2)
+        rotate_left(cube->data);
+    //right
+    if (x > W_WIDTH / 2)
+        rotate_right(cube->data);
+    //reset to center
+    mlx_mouse_move(cube->mlx, cube->mlx_win, W_WIDTH / 2, W_HEIGHT / 2);
+    return (0);
+}
